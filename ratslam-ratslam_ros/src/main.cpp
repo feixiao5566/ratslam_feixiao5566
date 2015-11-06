b7be60d84b2d2f8162c1deffa484abe7980e745f
feixiao5566 <feixiao5566@qq.com>
2015-11-06 17:21:20 +0800

    purg_map

--- ratslam-ratslam_ros/src/main.cpp
+++ ratslam-ratslam_ros/src/main.cpp
@@ -13,12 +13,11 @@
 using namespace std;
 
 typedef size_t utint;
-//定义一些全局数据,初始功能实现后,定义为类的全局变量 public
 int cur_id_ = 0;
 int pre_id_ = 0;
 
 template <class T,class V>
-class pair_first_comp  //初对比较
+class pair_first_comp
 
 {
 public:
@@ -34,71 +33,66 @@ template<class T>
 class graf
 {
 private:
-	vector<T> data;    //数组 data      
-	vector<list<pair <utint,int> > > links;     // 数组<链表<对组> >
-	utint edgeCount;     //边
+	vector<T> data;      
+	vector<list<pair <utint,int> > > links;
+	utint edgeCount;
 public:
-	graf( const vector<T>& _data)      //   构造函数
+	graf( const vector<T>& _data)
 	{
 		data=_data;
 		utint n=_data.size();
 		for(utint i=0;i<n;i++)
 		{
-			list<pair <utint,int> > temp;   // 第一个uint是data.size,第二个是vector的值
-			links.push_back(temp);  //存入数组
+			list<pair <utint,int> > temp;
+			links.push_back(temp);
 		}
 	}
-	//增加边连接
-	int addLink(utint from, utint to, int weight)   //    前个节点,后个节点 ,权
+	int addLink(utint from, utint to, int weight)
 	{
-		if(links.size()>from && links.size()>to)    //  ?
+		if(links.size()>from && links.size()>to)
 		{
-			links[from].push_back(pair<utint,int>(to,weight));     //  找到from,在from后链表加入to和权
+			links[from].push_back(pair<utint,int>(to,weight));
 			return 0;
 		}else{
 			return -1;
 		}
-		edgeCount++;    //边
+		edgeCount++;
 	}
-	//增加点连接
 	void addNode(const T& element)
 	{
-		data.push_back(element);   //点在data数组中
-		list<pair <utint,int> > temp;    //这对pair值哪里来的
+		data.push_back(element);
+		list<pair <utint,int> > temp;
 		links.push_back(temp);
 	}
-	//打印图
 	void printGraf()
 	{
 		for(utint i=0;i<links.size();i++)
 		{
 			cout<<data[i]<<endl;
 			list<pair<utint,int> >::iterator it=links[i].begin();
-			while(it!=links[i].end())//.end
+			while(it!=links[i].end())
 			{
 				cout<<data[i]<<" ->"<<data[(*it).first]<<" [ label = \""<<(*it).second<<"\"]"<<endl;
 				it++;
 			}
 		}
 	}
-	//直连路   直接节点路径
 	pair<bool,int> isDirectWay(utint from,utint to)
 	{
 		pair_first_comp<utint,int> cmp;
 		cmp.operand=to;
-		list<pair<utint,int> >::iterator it=find_if(links[from].begin(),links[from].end(),cmp);//it 结构体链表
-		if(links[from].end()==it)  //如果it到了链表的末尾
+		list<pair<utint,int> >::iterator it=find_if(links[from].begin(),links[from].end(),cmp);
+		if(links[from].end()==it)
 		{
-			return pair<bool,int>(false,0);  //返回假和0
+			return pair<bool,int>(false,0);
 		}else{
-			return pair<bool,int>(true,it->second); //返回真和他的下一个
+			return pair<bool,int>(true,it->second);
 		}
 	}
-	//dijkstar算法找最短路径
 	pair<bool,int> Dijkstra(utint from,utint to)// O(N^2)
-	{//using array
-		vector< pair<bool,int> > dist(data.size(), pair<bool,int>(0,-1));// array of lenght (bool=0 => unexplored)        这个数组叫dist  放着pair  为啥还有个size? 
-		vector<int> prev (data.size(),-1);//array of previous vertexes in the way  顶点集 数组 前一个
+	{
+		vector< pair<bool,int> > dist(data.size(), pair<bool,int>(0,-1));
+		vector<int> prev (data.size(),-1);
 		dist[from]=pair<bool,int>(1,0);
 		utint curr=from;
 		bool complete=false;
@@ -142,12 +136,10 @@ public:
 		}
 		return dist[to];
 	}
-	
-  //最短路径	
 	pair<bool,int> shortestWay(utint from,utint to)
-	{//Bellman-Ford alhorithm
-		 vector< pair<bool,int> > dist(data.size(), pair<bool,int>(0,0));// array of lenght (bool=0 => infinite)
-		 vector<int> prev (data.size(),-1);//array of previous vertexes in the way
+	{
+		 vector< pair<bool,int> > dist(data.size(), pair<bool,int>(0,0));
+		 vector<int> prev (data.size(),-1);
 		 dist[from]=pair<bool,int>(1,0);
 		 for(utint i=0;i<data.size()-1;i++)
 		 {
@@ -160,14 +152,12 @@ public:
 					 {
 						 if(dist[it->first].first)
 						 {
-							 //not inf
 							 if(dist[it->first].second>(dist[j].second+it->second))
 							 {
 								 dist[it->first].second=dist[j].second+it->second;
 								 prev[it->first]=j;
 							 }
 						 }else{
-							 //inf
 							 dist[it->first]=pair<bool,int>(1,dist[j].second+it->second);
 							 prev[it->first]=j;
 						 }
@@ -176,8 +166,6 @@ public:
 				 }
 			 }
 		 }
-		 
-		
 		 cout<<data[to]<<"<-";
 		 int p=prev[to];
 		 while(p!=-1)
@@ -214,15 +202,12 @@ int main(int argc, char **argv)
   ROS_INFO_STREAM("Distributed under the GNU GPL v3, see the included license file.");
   ROS_INFO_STREAM(" - feixiao ceshi.");
   
-  ros::init(argc, argv, "graph");//初始化
+  ros::init(argc, argv, "graph");
   ros::NodeHandle n;
   ros::Subscriber sub = n.subscribe("/irat_red/LocalView/Template", 0, node_callback);
-  //this may takes some troubles
-	//         graf<int> g(arr);  //graf类的构造函数  把数组和数组大小 做成链表 存入数组
 	ros::Rate loop_rate(1);
 	pre_id_ = 0;
 	arr.push_back(pre_id_);
-	//	vector<int> arr;
 	//for ()
 	arr.push_back(1);
 	arr.push_back(2);
@@ -232,51 +217,8 @@ int main(int argc, char **argv)
 	graf<int> g(arr);
 	cout<< "初始化成功\n";
 	while(ros::ok())
-  { 
-         
-  /*cout << "choose mode: 1--->way   2--->draw   3--->maps"<<endl;
-  cin >> chs_mode;
-  switch(chs_mode)
   {
-    case 1:{arr.push_back(cur_id_);
-           //arr.push_back(2);
-	         //arr.push_back(3);
-	         //arr.push_back(4);
-	         //arr.push_back(5);
-	         g.addLink(pre_id_, cur_id_, 3);
-	         //g.addLink(0,3,2);
-	         //g.addLink(1,4,2);
-	         //g.addLink(2,4,1);
-	         //g.addLink(3,2,1);
-	         //g.addLink(3,4,4);
-	
-	         //g.addLink(0,1,5);
-	         //g.addLink(0,3,10);
-	         //g.addLink(1,2,5);
-	         //g.addLink(1,4,1);
-	         //g.addLink(2,4,1);
-	         //g.addLink(3,2,-7);
-	         //g.addLink(3,4,-4);
-	         }
-	         break;
-	
-	  case 2:{cout<<"want to find head and tail is: "<<endl;
-	         cin>>head;
-	         cin>>tail;
-	         cout<<"shortest way: ";//<<g.shortestWay(head,tail).second<<endl;
-	
-	         cout<<"dijkstra: "<<g.Dijkstra(head,tail).second<<endl;
-	         cout<<"way: "<<g.isDirectWay(0,4).second<<endl;
-	         }
-	         break;
-	         
-	  case 3:g.printGraf();
-	         break;
-	  default: break;
-	
-	}*/
-	
-	ROS_INFO_STREAM("i don`t repeat\n");
+    ROS_INFO_STREAM("i don`t repeat\n");
 	cout<<"cur_id:"<< cur_id_<<endl;
 	//while
 	if(pre_id_ != cur_id_)
@@ -295,25 +237,9 @@ int main(int argc, char **argv)
 	    i = 0;
 	    //break;
 	  }
-	 // cout <<cur_id_<<pre_id_<<i<<endl;
+	 
 	}
-	/*
-	g.addLink(0,1,5);
-	g.addLink(0,3,10);
-	g.addLink(1,2,5);
-	g.addLink(1,4,1);
-	g.addLink(2,4,1);
-	g.addLink(3,2,-7);
-	g.addLink(3,4,-4);
-	
-	cout<<"shortest way: "<<g.shortestWay(0,4).second<<endl;
-	*/
-//	cout<<"want to find head and tail is: "<<endl;
-//	cin>>head;
-//	cin>>tail;
 	cout<<"dijkstra: "<<g.Dijkstra(1, 4).second<<endl;
-	
-//	g.printGraf();
 	cout<<"way: "<<g.isDirectWay(1, 4).second<<endl;
 	  ros::spinOnce();
 	 loop_rate.sleep();
